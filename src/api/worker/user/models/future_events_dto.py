d91bbe7bd1b50261c33f73564b437d7df82b2b1e
Tiago Narciso <95338528+almerion@users.noreply.github.com>
2024-01-08 09:54:24 +0100

    Update src/api/worker/user/models/future_events_dto.py
    Co-authored-by: Naulan.CHRZ <nonochrzaszcz@gmail.com>

--- src/api/worker/user/models/future_events_dto.py
+++ src/api/worker/user/models/future_events_dto.py
@@ -29,7 +29,7 @@ class FutureCarpoolingDTO:
         return asdict(self)
 
 
-@dataclass()
+@dataclass
 class FutureEventsDTO:
     reserved: List[FutureReservationDTO]
     proposed: List[FutureCarpoolingDTO]
