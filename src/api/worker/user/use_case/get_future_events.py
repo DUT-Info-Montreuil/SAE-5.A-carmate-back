c74989466f8047152079ed9006aeee7b50690e77
Tiago Narciso <95338528+almerion@users.noreply.github.com>
2024-01-08 09:54:33 +0100

    Update src/api/worker/user/use_case/get_future_events.py
    Co-authored-by: Naulan.CHRZ <nonochrzaszcz@gmail.com>

--- src/api/worker/user/use_case/get_future_events.py
+++ src/api/worker/user/use_case/get_future_events.py
@@ -8,22 +8,16 @@ from database.schemas import DriverProfileTable, UserTable
 
 
 class GetFutureEvents(Worker):
-    def worker(self, token: str):
-        user: UserTable = None
-        driver_profile: DriverProfileTable = None
-        future_reservations: List[FutureReservationDTO] = []
-        future_carpoolings: List[FutureCarpoolingDTO] = []
-
+    def worker(self, 
+               token: str):
         user = self.token_repository.get_user(sha512(token.encode()).digest())
-
+        future_reservations = self.booking_carpooling_repository.get_future_reservations_by_passenger_id(user.id)
+        
+        driver_profile: DriverProfileTable
+        future_carpoolings: List[FutureCarpoolingDTO]
         try:
             driver_profile = self.token_repository.get_driver_profile(sha512(token.encode()).digest())
-        except NotFound:
-            pass
-
-        future_reservations = self.booking_carpooling_repository.get_future_reservations_by_passenger_id(user.id)
-
-        if driver_profile is not None:
             future_carpoolings = self.carpooling_repository.get_future_carpoolings_by_driver_id(driver_profile.id)
-
+        except NotFound:
+            future_carpoolings = []
         return FutureEventsDTO(future_reservations, future_carpoolings)
