b62ad8f380decaaa53194bd350c56f19536a239e
NaulaN <nonochrzaszcz@gmail.com>
2024-01-16 10:26:59 +0100

    fix(repositories): change the name file to follow the conventionnal

--- src/database/repositories/__init__.py
+++ src/database/repositories/__init__.py
@@ -8,4 +8,4 @@ from .carpooling_repository import CarpoolingRepository
 from .user_admin_repository import UserAdminRepository
 from .user_banned_repository import UserBannedRepository
 from .review_repository import ReviewRepository
-from .propose_scheduled_carpooling import ProposeScheduledCarpoolingRepository
+from .propose_scheduled_carpooling_repository import ProposeScheduledCarpoolingRepository
